8d6d35d4ee96fe5649aff2e306856aef89a5d6fb
Trwifon <trifon@mail.com>
2024-10-29 21:46:16 +0200

    test exam

--- furry_funnies/common/views.py
+++ furry_funnies/common/views.py
@@ -4,12 +4,14 @@ from django.views.generic import ListView
 
 from furry_funnies.posts.models import Post
 
+from furry_funnies.furry_funnies.author.models import Author
+
 
 def index(request):
     return render(request, 'common/index.html')
 
 
 class Dashboard(ListView):
-    model = Post
+    model = Author
     template_name = 'common/dashboard.html'
     context_object_name = 'posts'
